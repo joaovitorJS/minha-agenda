3280bc629aa127b914cb916ec31c90c67283bdcc
jonny <joao.vitordeoliveira@hotmail.com>
2024-10-27 21:35:00 -0300

    feat: add inertia and react

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/login', function () {
+    return Inertia::render('login/index');
 });
