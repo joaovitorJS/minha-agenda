e1f4c6ac2e6d37467a76ec543f8c46b557274fd7
joaovitor <joao.vitordeoliveira@hotmail.com>
2024-11-03 22:17:13 -0300

    feat: logout user

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,16 +4,28 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class LoginController extends Controller
 {
+    /**
+     * @return Response
+     */
     public function index()
     {
         return Inertia::render('login/index');
     }
 
+    /**
+     * @param LoginRequest $request
+     * 
+     * @return Redirector|RedirectResponse
+     */
     public function login(LoginRequest $request)
     {
         $request->authenticate();
@@ -21,5 +33,22 @@ class LoginController extends Controller
         $request->session()->regenerate();
 
         return redirect(route('dashboard'));
+    }
+
+
+    /**
+     * @param Request $request
+     * 
+     * @return Redirector|RedirectResponse
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect(route('login.index'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,7 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 });
